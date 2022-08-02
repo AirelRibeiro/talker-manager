3cb80a7d7a2aaead549605a6cf4af756e6248157
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-02 18:44:22 -0300

    fix: lint

--- errorMiddleware.js
+++ errorMiddleware.js
@@ -1,9 +1,9 @@
-const errorMiddleware = (err, req, res, next) => {
+const errorMiddleware = (err, req, res, _next) => {
   if (err.status) {
     return res.status(err.status).json({ message: err.message });
   }
 
   return res.status(500).json({ message: err.message });
-}
+};
 
 module.exports = errorMiddleware;
