b7ee3d24e7dcc91c888e5f077389a6fef2115aad
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-02 16:43:22 -0300

    feat: inclui requires de auxiliares

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
+const crypto = require('crypto');
+const {
+  validatePersonalInfors,
+  validateToken,
+  validadeName,
+  validateAge,
+  validateTalk,
+} = require('./validationMiddlewares');
+const errorMiddleware = require('./errorMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
